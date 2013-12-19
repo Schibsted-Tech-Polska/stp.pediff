d6e30b0015716131294554df8e27383808fb1e25
Krzysztof Kula <krzychukula@gmail.com>
2013-12-19 16:56:21 +0100

    make it more configurable

--- pages.js
+++ pages.js
@@ -1,24 +1,36 @@
-exports.pages = ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'];
+exports.pages = function(pages){
+	var arr = [];
+	var i = '1';
+	do{
+		arr.push('page'+i);
+	}while(i++ < pages);
+	return arr;
+}
+
+// console.log(exports.pages(0)) // -> [ 'page1' ]
+// console.log(exports.pages(1)) // -> [ 'page1' ]
+// console.log(exports.pages(2)) // -> [ 'page1', 'page2' ]
+// console.log(exports.pages(3)) // -> [ 'page1', 'page2', 'page3' ]
+// console.log(exports.pages(4)) // -> [ 'page1', 'page2', 'page3', 'page4' ]
 
 exports.exec = function(){
     this.then(function(response){
 
         var withoutPage = response.url.replace('&page=0', '&page=');
-        var urls = exports.pages.map(function(el){
+        var urls = this.config.actions.map(function(el){
         	return el.replace('page', withoutPage);
         });
-        //[withoutPage+'1', withoutPage+'2', withoutPage+'3', withoutPage+'4', withoutPage+'5'];
 
         function runMore(){
-        		this.wait(1000, function(){
+        		this.wait(2000, function(){
 	            this.save();
 
 	            this.eachThen(urls, function(response) {
 	                this.thenOpen(response.data, function(response) {
 
-	                    this.waitUntilVisible('.alf-template', savePage, savePage, 9000);
+	                    this.waitUntilVisible('.alf-template', savePage, savePage, 5000);
 	                    function savePage () {
-	                    	this.wait(1000, function(){
+	                    	this.wait(2000, function(){
 	                        this.save('page'+response.url.slice(-1));
 	                        console.log('Opened', response.url);
 	                      });
@@ -29,7 +41,7 @@ exports.exec = function(){
 	           });
         };
 
-        this.waitUntilVisible('.alf-template', runMore.bind(this), runMore.bind(this), 15000);
+        this.waitUntilVisible('.alf-template', runMore.bind(this), runMore.bind(this), 5000);
     })
 
 }
--- tasks/video-ipad.js
+++ tasks/video-ipad.js
@@ -10,7 +10,7 @@ module.exports = {
             print: false
         },
         package: 'ipadvideocover',
-        actions: task.pages
+        actions: task.pages(3)
     },
     execute: task.exec
 };
