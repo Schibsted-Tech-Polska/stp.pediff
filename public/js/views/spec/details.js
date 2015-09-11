17a3d9c210fcc36f9814412d7777ef9f90e887a2
kjakubik <krzysiek.jakubik@gmail.com>
2015-09-11 15:51:50 +0200

    Add spec url to details view

--- public/js/views/spec/details.js
+++ public/js/views/spec/details.js
@@ -1,15 +1,23 @@
 define([
+    'utils',
     'text!templates/spec/details.html'
-], function(viewTemplate) {
+], function(utils, viewTemplate) {
     var View = Backbone.View.extend({
         events: {},
+        initialize: function() {
+        },
         destroy: function() {
             this.undelegateEvents();
             this.stopListening();
             this.$el.empty();
         },
         render: function() {
-            this.$el.html(_.template(viewTemplate));
+            var report = utils.storage.use('application').get('report');
+
+            this.$el.html(_.template(viewTemplate, {
+                model: this.model,
+                environments: report.config.environments
+            }));
         }
     });
     return View;
