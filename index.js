b40455a1e663d1e84c82e732fd8e34b47583d799
kjakubik <krzysiek.jakubik@gmail.com>
2015-09-15 10:02:05 +0200

    Add command line support

--- index.js
+++ index.js
@@ -1,28 +1,65 @@
+#!/usr/bin/env node
+
 var Pediff = require('./lib/pediff.js'),
     Server = require('./lib/server.js'),
     parseConfig = require('./lib/config.js'),
-    config = require('./config.js'),
+    meow = require('meow'),
+    config,
     instance;
 
-// TODO: command line configuration
-// TODO: load pediff.js as config file by default
-// TODO: running individual specs from command line
-
 var defaults = {
-    port: 5000,
     specDir: 'spec',
     resultsDir: 'results',
     parallelLimit: 18,
-    debug: false,
     live: false
 };
 
+var cli = meow({
+    help: [
+        'Usage',
+        '  pediff [options] run all',
+        '  pediff [options] run <spec>[ <spec2> <spec...> ]',
+        '',
+        'Options',
+        //'  --live          - runs a webserver for dynamic testing',
+        '  --config <path> - tells pediff where to look for a configuration file (by default it\'s pediff.js in root directory)',
+        '  --debug         - outputs additional information'
+    ]
+});
+
+if(!cli.input.length && !cli.flags.live || cli.input[0] !== 'run' || !cli.input[1]) {
+    cli.showHelp();
+    process.exit(0);
+}
+
+try {
+    if(cli.flags.config) {
+        config = require(cli.flags.config);
+    } else {
+        config = require('pediff.js');
+    }
+} catch (e) {
+    if(cli.flags.config) {
+        console.error('  Configuration file ' + cli.flags.config + ' not found.');
+    } else {
+        console.error('  Configuration file pediff.js not found.\n  Please create this file in the root directory, or tell us where to find it with the --config option.');
+    }
+    process.exit(0);
+}
+
 config = parseConfig(config, defaults);
+
+config.debug = !!cli.flags.debug;
 
 instance = new Pediff(config);
 
-if(config.live) {
-    new Server(config, instance);
-} else {
-    instance.runAll();
-}
+//if(cli.flags.live) {
+//    new Server(config, instance);
+//} else {
+    if(cli.input[1] === 'all') {
+        instance.runAll();
+    } else {
+        var specs = cli.input.splice(1, cli.input.length - 1);
+        instance.runBundle(specs);
+    }
+//}
